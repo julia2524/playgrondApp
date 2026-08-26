595d9e7776db75c3682381a95af0e6d8a162a591
julia2524 <61957239+julia2524@users.noreply.github.com>
2026-08-26 14:07:54 +0900

    homescreen connect to game

--- games/classification/color/components/GameHeader.tsx
+++ games/classification/color/components/GameHeader.tsx
@@ -1,3 +1,4 @@
+import { TouchableOpacity } from "react-native";
 import {
   BackText,
   Header,
@@ -5,18 +6,28 @@ import {
   TitleText,
 } from "../styles/classificationStyles";
 import { ClassificationLevel } from "../types";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../../../navigation/types";
+import { useNavigation } from "@react-navigation/native";
 
 interface GameHeaderProps {
   levelConfig: ClassificationLevel;
   roundIndex: number;
 }
+type GameHeaderNavigationProp = NativeStackNavigationProp<RootStackParamList>;
 export default function GameHeader({
   levelConfig,
   roundIndex,
 }: GameHeaderProps) {
+  const navigation = useNavigation<GameHeaderNavigationProp>();
+  const goToHome = () => {
+    navigation.navigate("Home");
+  };
   return (
     <Header>
-      <BackText>‹</BackText>
+      <TouchableOpacity onPress={goToHome} activeOpacity={0.7}>
+        <BackText>‹</BackText>
+      </TouchableOpacity>
       <TitleText>🎨 색깔 분류 (LEVEL {levelConfig.level})</TitleText>
       <RoundIndicator>{roundIndex + 1} / 5</RoundIndicator>
     </Header>
--- navigation/types.ts
+++ navigation/types.ts
@@ -1,3 +1,6 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+// 1. 전체 라우트 파라미터 타입 정의 (Home과 게임 화면 추가)
 export type RootStackParamList = {
   Home: undefined;
   ClassificationPlayScreen: undefined;
